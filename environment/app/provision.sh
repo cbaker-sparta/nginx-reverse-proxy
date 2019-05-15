b43fb62d711f89aa0047f3a6d391cd420d6a382f
Tech-A24 <tech-a24@Tech-A24s-MacBook-Air.local>
2019-05-15 21:49:44 +0100

    first commit

---
+++ environment/app/provision.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Update the sources list
+sudo apt-get update -y
+
+# upgrade any packages available
+sudo apt-get upgrade -y
+
+# install nginx
+sudo apt-get install nginx -y
+
+# install git
+sudo apt-get install git -y
+
+# install nodejs
+sudo apt-get install python-software-properties
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get install nodejs -y
+sudo npm install pm2 -g
+
+sudo unlink /etc/nginx/sites-enabled/default
+cd /etc/nginx/sites-available
+sudo touch reverse-proxy.conf
+sudo chmod 766 reverse-proxy.conf
+echo "server {
+  listen 80;
+
+  location / {
+    proxy_pass http://192.168.10.100:3000;
+  }
+}" >> reverse-proxy.conf
+
+sudo ln -s /etc/nginx/sites-available/reverse-proxy.conf /etc/nginx/sites-enabled/reverse-proxy.conf
+
+sudo service nginx configtest
+sudo service nginx restart
